d4f5a9ec7581fc66c30e8c8866a4bba214bf8260
Phyu Phyu Thin <phyuthin2004@gmail.com>
2022-09-15 11:08:51 +0630

    comment entity

--- src/main/java/com/ai/controller/StudentController.java
+++ src/main/java/com/ai/controller/StudentController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import javax.servlet.annotation.MultipartConfig;
 
