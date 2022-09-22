a6afe675e18c53a4c7fd5dc3418194692a3fbd65
Phyu Phyu Thin <phyuthin2004@gmail.com>
2022-09-22 13:52:56 +0630

    AnswerServiceTest

--- src/test/java/com/ai/ServiceTest/AnswerServiceTest.java
+++ src/test/java/com/ai/ServiceTest/AnswerServiceTest.java
@@ -1,10 +1,15 @@
 package com.ai.ServiceTest;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.ai.entity.Answer;
+import com.ai.entity.Question;
 import com.ai.repository.AnswerRepository;
 import com.ai.service.AnswerService;
 
@@ -16,4 +21,21 @@ public class AnswerServiceTest {
 
     @InjectMocks
     AnswerService answerService;
+
+    public Answer answerObj(){
+        Answer answer = Answer.builder()
+        .id(1)
+        .answer("answer")
+        .question(new Question())
+        .build();
+        return answer;
+    }
+    
+    @Test
+    public void saveTest(){
+        Answer answer = answerObj();
+        answerService.save(answer);
+        verify(answerRepository, times(1)).save(answer);
+    }
+
 }
